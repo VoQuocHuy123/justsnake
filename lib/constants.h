dfc04c0b0cddbd5a03dc61e1eb8c5c03cf081607
VoQuocHuy123 <127522528+VoQuocHuy123@users.noreply.github.com>
2023-04-27 21:11:20 +0700

    Add files via upload

---
+++ lib/constants.h
@@ -0,0 +1,25 @@
+// DO NOT CHANGE THIS FILE
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+    #include <SDL.h>
+ #include <bits/stdc++.h>
+
+ using namespace std;
+
+
+const int SCREEN_WIDTH = 900;
+const int SCREEN_HEIGHT = 600;
+const char WINDOW_TITLE[] = "Snake Game";
+
+const int BOARD_WIDTH = 30;
+const int BOARD_HEIGHT = 20;
+const int CELL_SIZE = 30;
+
+const SDL_Color BOARD_COLOR = {0, 0, 0};
+const SDL_Color LINE_COLOR = {128, 128, 128};
+
+const double STEP_DELAY = 0.2;
+
+#endif
